e9b88f0f8aff437c4c18f02c4ad7f05368e288e2
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-05-27 10:12:40 +0800

    fix code

--- app/script/src/deal.rs
+++ app/script/src/deal.rs
@@ -1,38 +1,36 @@
 pub mod Deal_ZKP {
 
     use sp1_core::{stark::ShardProof, utils::BabyBearPoseidon2};
-    use sp1_sdk::{client, utils, ProverClient, SP1ProofWithMetadata, SP1Stdin};
+    use sp1_sdk::{utils, ProverClient, SP1ProofWithMetadata, SP1Stdin};
     use std::fs::File;
     use std::io::BufReader;
 
     const APP_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
     pub async fn deal_zkp() {
-        // utils::setup_tracer();
-        // // Generate proof.
-        // let mut stdin = SP1Stdin::new();
-        // let n = 186u32;
-        // stdin.write(&n);
+
+        utils::setup_tracer();
+        // Generate proof.
+        let mut stdin = SP1Stdin::new();
+        let n = 186u32;
+        stdin.write(&n);
         let client = ProverClient::new();
         let (_, vk) = client.setup(APP_ELF);
 
         // let mut proof = client.prove(&pk, stdin).expect("proving failed");
 
-        // // Read output.
-        // let a = proof.public_values.read::<u128>();
-        // let b = proof.public_values.read::<u128>();
-        // println!("a: {}", a);
-        // println!("b: {}", b);
-
-        // // Verify proof.
-        // client.verify(&proof,&vk).expect("verification failed");
+        // Verify proof.
+        // client.verify(&proof, &vk).expect("verification failed");
 
         // // Save proof.
         // proof
         // .save("proof-with-pis.json")
         // .expect("saving proof failed");
 
-        let file = File::open("proof-with-pis.json").expect("read failed");
+        // let proof_for_sp1 = serde_json::to_string(&proof).expect("Unable to serialize data");
+        // std::fs::write("proof.json", proof_for_sp1).expect("Unable to write file");
+
+        let file = File::open("proof.json").expect("read failed");
         let reader = BufReader::new(file);
         let proof: SP1ProofWithMetadata<Vec<ShardProof<BabyBearPoseidon2>>> =
             serde_json::from_reader(reader).expect("deserialization failed");
